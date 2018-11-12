98121a49c664ed9ea838a664c54c44ad528525ff
volodymyr.puchkov <volodymyr.puchkov@onereach.com>
2018-11-12 18:52:42 +0200

    Variable name component creation

---
+++ src/editor/email.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="email-wrapper">
+        <div class="email-input-section">
+            <or-textbox v-model="displayValue" class="email-input"
+                        :label="label"
+                        :placeholder="placeholder"
+                        :disabled="readonly"
+                        @input="$v.schema.email.$touch()"
+                        :invalid="$v.schema.email.$error"
+                        error="Email is required">
+            </or-textbox>
+        </div>
+    </div>
+</template>
+<script>
+    import * as _ from 'lodash'
+
+    export default {
+        props    : ['schema', 'label', 'placeholder', 'readonly', '$v', 'variableName'],
+        computed : {
+            displayValue : {
+                get () {
+                    return _.get(this.schema, this.variableName, '');
+                },
+                set (newValue) {
+                    this.schema[this.variableName] = newValue;
+                }
+            }
+        }
+    }
+</script>
+
+
+<style scoped lang="scss" rel="stylesheet/scss">
+    @import '../scss/colors.scss';
+    @import '../scss/fonts.scss';
+
+    .email-wrapper {
+        .email-input-section {
+            .email-input {
+
+            }
+        }
+    }
+
+</style>
+
+<style lang="scss" rel="stylesheet/scss">
+    @import '../scss/colors.scss';
+
+</style>
---
+++ src/input/input.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="example-input-component-wrapper">
+        <div class="wrapper" @click="$refs.configModal.open()">
+            <editor :template="input.data"
+                    :schema="defaultStep.data"
+                    :step="defaultStep"
+                    :steps="[defaultStep]"
+                    :readonly="true">
+            </editor>
+        </div>
+
+        <or-modal ref="configModal" :remove-close-button="true"
+                  title="Configure component"
+                  class="input-message-settings"
+                  :contain-focus="false">
+            <or-textbox name="label" label="Variable name" placeholder="Variable name"
+                        v-model="input.data.variableName"
+                        help="input label text">
+            </or-textbox>
+
+            <div slot="footer">
+                <or-button color="primary"
+                           type="secondary"
+                           @click.prevent="$refs.configModal.close()">
+                    Close
+                </or-button>
+            </div>
+        </or-modal>
+    </div>
+</template>
+
+<script>
+    import _ from 'lodash';
+    import base from '@default/src/inputs/_design_base.vue';
+    import editor from '../editor/editor.vue';
+
+    export default {
+        extends    : base,
+        components : {
+            editor
+        },
+        defaultValue () {
+            return _.cloneDeep(data);
+        }
+    };
+
+    export const label = 'Test example';
+    export const data = {
+        email               : '',
+        emailPlaceholder    : '',
+        emailLabel          : '',
+        password            : '',
+        passwordPlaceholder : '',
+        passwordLabel       : '',
+        validateRequired    : true
+    };
+
+    export const meta = {
+        name    : 'test-external-component',
+        type    : 'onereach-studio-form-input',
+        version : '1.0'
+    };
+</script>
